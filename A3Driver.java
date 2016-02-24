164231fd04983d65796919f462ffc19514f739dd
dylankeeton <dylan.keeton@yahoo.com>
2016-02-23 18:04:59 -0600

    Added files via upload

--- A3Driver.java
+++ A3Driver.java
@@ -13,27 +13,48 @@ public class A3Driver
 			  System.err.println ("Error: Incorrect number of command line arguments");
 			  System.exit(-1);
 		  } 
-		  try 
-			{
-				FileReader freader = new FileReader(args[0]);
-				BufferedReader reader = new BufferedReader(freader);
-				for (String s = reader.readLine(); s != null; s = reader.readLine()) 
-				{
-					try{
-						String[] input = s.split(" ");
-						switch(input[0])
-						{
-						case "insert" : break;
-						case "search" : break;
+		  try {
+			  FileReader freader = new FileReader(args[0]);
+			  BufferedReader reader = new BufferedReader(freader);
+			  for (String s = reader.readLine(); s != null; s = reader.readLine()) 
+			  {
+				  try{
+					  String[] input = s.split(" ");
+					  switch(input[0])
+					  {
+					  	case "insert" : insertItem(input, shoppingCart); break;
+						case "search" : searchItem(input, shoppingCart); break;
 						case "delete" : deleteItem(input, shoppingCart); break;
 						case "update" : updateItem(input, shoppingCart); break;
-						case "print" : System.out.println("Hello");break;
-						default: throw new InvalidCategoryException("Invalid Category");
+						case "print" : 
+						{
+							if(input.length != 1)
+								throw new InvalidCategoryException("Invalid Category");
+							double total = 0;
+							Iterator<Item> i = shoppingCart.iterator();
+							while (i.hasNext()) 
+							{
+								Item temp = i.next();
+								total += temp.calculatePrice(); 
+								temp.printItemAttributes();
+							}	
+							System.out.printf("Total Shopping Cart Charge: $%.02f\n", total);
+							break;
 						}
-					} catch(InvalidCategoryException a){
-						
-						System.err.println("INVALID CATEGORY, COMMAND ABORTED");
+						default: throw new InvalidCategoryException("Invalid Category");
+					  }
+					} 
+					catch(InvalidCategoryException a){
+						System.err.println("INVALID INPUT, COMMAND ABORTED");
 					}
+					catch(ArrayIndexOutOfBoundsException a)
+					{	
+						System.err.println("INVALID INPUT, COMMAND ABORTED");
+					}
+				  	catch(NumberFormatException a)
+				  	{
+				  		System.err.println("INVALID INPUT, COMMAND ABORTED");
+				  	}
 				}
 			} 
 			catch (FileNotFoundException e) 
@@ -48,7 +69,7 @@ public class A3Driver
 				System.exit(-1);
 			}
 		
-		// General code example for how to iterate an array list. You will have to modify this heavily, to suit your needs.
+		/* General code example for how to iterate an array list. You will have to modify this heavily, to suit your needs.
 		  Iterator<Item> i = shoppingCart.iterator();
 		  while (i.hasNext()) 
 		  {
@@ -58,50 +79,141 @@ public class A3Driver
 			  //This (above) works because of polymorphism: a determination is made at runtime, 
 			  //based on the inherited class type, as to which method is to be invoked. Eg: If it is an instance
 			  // of Grocery, it will invoke the calculatePrice () method defined in Grocery.
-		  }		
-
+		  }	
+		  
+		  
+		*/
 	  }
 	  
+	  static void insertItem(String[] input, ArrayList<Item> sc)
+	  {
+		  try{
+			  String[] states = {"AK", "AL", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN",
+			  			 "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV",
+			  			 "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN",
+			  			 "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"};
+			  if(Double.valueOf(input[3]) < 0 || Integer.valueOf(input[4]) < 0 || Integer.valueOf(input[5]) < 0)
+				  throw new InvalidCategoryException("Invalid Category");
+			  switch(input[1])
+			  {
+				case "clothing": 
+				{
+					if(input.length == 6)
+						sc.add(new Clothing(input[2],Double.valueOf(input[3]),Integer.valueOf(input[4]),Integer.valueOf(input[5]))); 
+					else
+						throw new InvalidCategoryException("Invalid Category");
+					break;
+				}
+				case "electronics":
+				{
+					if(Arrays.asList(states).contains(input[7]))
+					{
+						if(input[6].equals("F") && input.length == 8)
+							sc.add(new Electronics(input[2],Double.valueOf(input[3]),Integer.valueOf(input[4]),Integer.valueOf(input[5]),true,input[7]));
+						else if(input[6].equals("NF") && input.length == 8)
+							sc.add(new Electronics(input[2],Double.valueOf(input[3]),Integer.valueOf(input[4]),Integer.valueOf(input[5]),false,input[7]));
+						else
+							throw new InvalidCategoryException("Invalid Category");
+					}
+					else
+						throw new InvalidCategoryException("Invalid Category");
+				}
+				case "groceries":
+				{
+					if(input[6].equals("P") && input.length == 7)
+						sc.add(new Grocery(input[2],Double.valueOf(input[3]),Integer.valueOf(input[4]),Integer.valueOf(input[5]),true));
+					else if(input[6].equals("NP") && input.length == 7)
+						sc.add(new Grocery(input[2],Double.valueOf(input[3]),Integer.valueOf(input[4]),Integer.valueOf(input[5]),false));					
+					else
+						throw new InvalidCategoryException("Invalid Category");
+				}
+				default: throw new InvalidCategoryException("Invalid Category");
+			}
+		 }
+		 catch(InvalidCategoryException a)
+		 {	
+			System.err.println("INVALID INPUT, COMMAND ABORTED");
+		 }
+		 catch(ArrayIndexOutOfBoundsException a)
+		 {
+			System.err.println("INVALID INPUT, COMMAND ABORTED");
+		 }
+		 catch(NumberFormatException a)
+		 {
+		  	System.err.println("INVALID INPUT, COMMAND ABORTED");
+		 }
+		 
+	 }
+	  
+	 static void searchItem(String[] input, ArrayList<Item> sc)
+	 {
+		 try{
+			 if(input.length != 2)
+					throw new InvalidCategoryException("Invalid Category");
+				else
+				{
+					int total = 0;
+					for(int i = 0; i < sc.size(); i++)
+					{
+						if(sc.get(i).getName().equals(input[1]))
+							total += sc.get(i).getQuantity();
+					}
+					System.out.printf("Total Quantity of %s: %d\n", input[1], total);
+				}
+		 }
+		 catch(InvalidCategoryException a)
+		 {	
+			System.err.println("INVALID INPUT, COMMAND ABORTED");
+		 }
+		 
+	 }
 	  
 	  static void deleteItem(String[] input, ArrayList<Item> sc)
 	  {
-		  if(input.length != 2){
-			  System.out.println("INVALID INPUT");
-			  return;
+		  try{
+			  if(input.length != 2){
+				  throw new InvalidCategoryException("Invalid Input");
+			  }
+			  
+			  int count = 0;
+			  String name = input[1];
+			  int i = 0;
+			
+			  while(i < sc.size()){
+				 if(name.equals(sc.get(i).getName())){
+					 count += sc.get(i).getQuantity();
+					 sc.remove(i);
+					 i--;
+				 }
+				 i++; 
+			  }
+			  System.out.println("Name: " + input[1] + "  Quantity Deleted: " + count);
 		  }
-		  
-		  int count = 0;
-		  String name = input[1];
-		  int i = 0;
-		  
-		  while(i < sc.size()){
-			 if(name.equals(sc.get(i).getName())){
-				 count += sc.get(i).getQuantity();
-				 sc.remove(i);
-				 i--;
-			 }
-			 i++; 
+		  catch(InvalidCategoryException a)
+		  {	
+			System.err.println("INVALID INPUT, COMMAND ABORTED");
 		  }
-		  
-		  System.out.println("Name: " + input[1] + "  Quantity Deleted: " + count);
 		  
 	  }
 	  
 	  static void updateItem(String[] input, ArrayList<Item> sc)
 	  {
-		  if(input.length != 3){
-			  System.out.println("INVALID INPUT");
-			  return;
-		  }
-		  
 		  int quantity = 0;
 		  try {
-			quantity = Integer.parseInt(input[2]);  
+			  if(input.length != 3){
+				  throw new InvalidCategoryException("Invalid Input");
+			  }  
+			  quantity = Integer.parseInt(input[2]);  
 		  }
 		  catch (NumberFormatException e) {
 			  System.out.println("quantity number is not valid");
 			  return;
 		  }
+		  catch(InvalidCategoryException a)
+		  {	
+			System.err.println("INVALID INPUT, COMMAND ABORTED");
+		  }
+		  
 		  
 		  String name = input[1];
 		  boolean nameFound = false;
@@ -112,20 +224,14 @@ public class A3Driver
 				  nameFound = true;
 				  break;
 			  }
-		  }
-		  
+		  }	  
 		  if(nameFound == false)
 		  {
 			  System.out.println("name not found.  invalid command");
 			  return;
-		  }
-		  
-		  sc.get(i).setQuantity(quantity);
-		  
-		  System.out.print("Name: " + input[1] + "  Updated Quantity: " + input[2]);
-		  
-		  
-			 
+		  }	  
+		  sc.get(i).setQuantity(quantity);	  
+		  System.out.print("Name: " + input[1] + "  Updated Quantity: " + input[2]);	 
 	  }
 	  
 }
